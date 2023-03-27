7572f44e4295aeab7bc61f025761d81593e97f32
Will O'Hara <william.ohara@mymail.champlain.edu>
2023-03-26 21:31:52 -0400

    Added tongue Grappling System

--- Unity Project/Space Frog Grappler/Assets/Scripts/PlayerInput.cs
+++ Unity Project/Space Frog Grappler/Assets/Scripts/PlayerInput.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private float turntableMod = 875000;
     [SerializeField] private float keyboardMod = 250;
+    public static event Action FireTongue;
+    public static event Action RetractTongue;
     Gamepad gamepad = Gamepad.current;
     // Start is called before the first frame update
     void Start()
@@ -18,21 +21,23 @@ public class PlayerInput : MonoBehaviour
     void Update()
     {
         gamepad.SetMotorSpeeds(0, Mathf.Sin(Time.time*4.5f) + 0.2f); //Makes tongue button pulse
+        if(Input.GetButtonDown("Tongue"))
+        {
+            FireTongue?.Invoke();
+        }
+        if (Input.GetButtonUp("Tongue"))
+        {
+            RetractTongue?.Invoke();
+        }
     }
 
     private void FixedUpdate()
     {
-        if (!(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)))
+        if (!(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)))
             transform.eulerAngles += new Vector3(0, 0, (Input.GetAxisRaw("Vertical") * -turntableMod * Time.fixedDeltaTime));
         else
         {
             transform.eulerAngles += new Vector3(0, 0, (Input.GetAxisRaw("Vertical") * -keyboardMod * Time.fixedDeltaTime));
         }
-        if(Input.GetButton("Tongue")) //Unity is having serious problems reading GetButtonDown (as in it's only reading like 1 in 10 presses), so I'm using GetButton for now.
-                                      //Shouldn't be a problem, since the tongue ability already has an inherent cooldown.
-        {
-            //Some event stuff
-        }
-
     }
 }
