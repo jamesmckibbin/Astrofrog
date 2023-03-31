0cfd1f671471ecfecd5c6824ee4975683d56cb61
James <james.mckibbin@mymail.champlain.edu>
2023-03-31 01:48:33 -0400

    Created oxygen meter

---
+++ Unity Project/Space Frog Grappler/Assets/Scripts/UI/Scr_Oxygen_Meter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_Oxygen_Meter : MonoBehaviour
+{
+    public float OxygenValue;
+
+    private Slider oxygenMeter;
+    private float maxOxygenValue = 120;
+    private float totalOxygenAdded = 0;
+    private float totalOxygenRemoved = 0;
+
+    // Grabs Slider component located on OxygenMeter
+    void Start()
+    {
+        oxygenMeter = GetComponent<Slider>();
+    }
+
+    // Adjusts the oxygen meter and sets the bar on the UI
+    void Update()
+    {
+        oxygenMeter.maxValue = maxOxygenValue;
+        OxygenValue = maxOxygenValue + totalOxygenAdded - totalOxygenRemoved - Time.timeSinceLevelLoad;
+        oxygenMeter.value = OxygenValue;
+    }
+
+    // Updates the maximum oxygen
+    public void SetMaxOxygen(float newvalue)
+    {
+        maxOxygenValue = newvalue;
+    }
+
+    // Adds oxygen to the bar
+    public void AddOxygen(float value)
+    {
+        totalOxygenAdded += value;
+    }
+
+    // Removes oxygen from the bar
+    public void RemoveOxygen(float value)
+    {
+        totalOxygenRemoved += value;
+    }
+}
