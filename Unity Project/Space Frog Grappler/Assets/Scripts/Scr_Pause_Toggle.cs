209b4692e0d51b46e2e1c2df36078267a5f1384a
Natalie Froelich <natfroelich@gmail.com>
2023-04-06 17:44:45 -0400

    More pause stuff

---
+++ Unity Project/Space Frog Grappler/Assets/Scripts/Scr_Pause_Toggle.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Pause_Toggle : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
