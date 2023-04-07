d058d406f3bdea9aba6b1fa7e3542f2160d08788
James <james.mckibbin@mymail.champlain.edu>
2023-04-07 01:59:05 -0400

    Scene switching implemented

---
+++ Unity Project/Space Frog Grappler/Assets/Scripts/ScripObjs/SceneInfo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SceneInfo : ScriptableObject
+{
+    // Please update these to the scene names when creating and implementing new scenes
+    // Write them in descending order from first scene to last scene
+
+    public int TitleScreen;
+    public int Credits;
+    public int Level_1;
+    public int Level_2;
+    public int Level_3;
+    public int GameOver;
+    public int EndScreen;
+}
