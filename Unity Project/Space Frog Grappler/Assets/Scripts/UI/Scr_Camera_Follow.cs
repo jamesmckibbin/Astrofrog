e912935f24e1566594cb7f4d37f3bf8ab8768e27
James <james.mckibbin@mymail.champlain.edu>
2023-04-16 16:42:36 -0400

    Added transition screens and fixed level camera

--- Unity Project/Space Frog Grappler/Assets/Scripts/UI/Scr_Camera_Follow.cs
+++ Unity Project/Space Frog Grappler/Assets/Scripts/UI/Scr_Camera_Follow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Scr_Camera_Follow : MonoBehaviour
 {
--- Unity Project/Space Frog Grappler/Assets/Scripts/UI/Scr_Scene_Switcher.cs
+++ Unity Project/Space Frog Grappler/Assets/Scripts/UI/Scr_Scene_Switcher.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Scr_Scene_Switcher : MonoBehaviour 
 {
+    [SerializeField] private Animator transition;
     private static Scr_Scene_Switcher instance;
     void Start()
     {
@@ -13,12 +14,14 @@ public class Scr_Scene_Switcher : MonoBehaviour
 
         else
         {Object.Destroy(this.gameObject);}
+
+        transition.SetTrigger("Start");
     }
 
     public IEnumerator LoadScene(int levelIndex)
     {
+        transition.SetTrigger("End");
         yield return new WaitForSeconds(1.0f);
-
         SceneManager.LoadScene(levelIndex);
     }
 }
