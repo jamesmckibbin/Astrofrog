490fa4e2e5300bba4c08ba127003959b5f2c2411
Natalie Froelich <natfroelich@gmail.com>
2023-04-17 17:55:48 -0400

    Level end screen stats complete

--- Unity Project/Space Frog Grappler/Assets/Scripts/Scr_Stat_Tracking.cs
+++ Unity Project/Space Frog Grappler/Assets/Scripts/Scr_Stat_Tracking.cs
@@ -40,26 +40,32 @@ public class Scr_Stat_Tracking : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name.Contains("Sce_Level_1"))
         {fliesEaten = GameObject.Find("Text_Fly_Counter").GetComponent<Scr_Fly_Counter>().fliesEaten;}
+        
+        print("flies eaten: " + fliesEaten);
+        
+        if (SceneManager.GetActiveScene().name == "Sce_Game_Over" || SceneManager.GetActiveScene().name == "Sce_Complete_Level")
+        {TallyScores();}
+
+        else if (SceneManager.GetActiveScene().name == "Sce_Select_Level")
+        {}
     }
 
     //Gives its numbers to the level complete / game over screen.
     //Then it updates ScoreInfo if necessary and destroys itself.
-    void OnSceneLoaded()
+    void TallyScores()
     {
-        if (SceneManager.GetActiveScene().name == "Sce_Game_Over" || SceneManager.GetActiveScene().name == "Sce_Complete_Level")
-        {
-            //flyText = GameObject.Find("Text_Final_Score").GetComponent<TextMeshProUGUI>();
+        print("uh huh, uh huh...");
 
-            print(fliesEaten.ToString() + " / " + flyTotal.ToString() + "flies!");
+        print(fliesEaten.ToString() + " / " + flyTotal.ToString() + "flies!");
 
-            flyText = GameObject.Find("Text_Final_Score").GetComponent<TextMeshProUGUI>();
+        flyText = GameObject.Find("Text_Final_Score").GetComponent<TextMeshProUGUI>();
             
-            flyText.text = fliesEaten.ToString() + " / " + flyTotal.ToString() + " flies!";
-        }
+        flyText.text = fliesEaten.ToString() + " / " + flyTotal.ToString() + " flies!";
 
         switch (whatLevel)
         {
             case 1:
+                print("i was on level 1!");
                 scoreIndexes.MaxLevel1 = flyTotal;
 
                 if (fliesEaten > scoreIndexes.BestLevel1)
@@ -81,6 +87,7 @@ public class Scr_Stat_Tracking : MonoBehaviour
                 break;
 
             default:
+                print("this reminds me of a novel by george orwell");
                 break;
         }
 
