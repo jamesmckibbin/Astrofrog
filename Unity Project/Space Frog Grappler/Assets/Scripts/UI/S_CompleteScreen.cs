c6134ceaf9890fc7f32f7ab8805d16a3fafc5c08
James <james.mckibbin@mymail.champlain.edu>
2023-04-29 23:57:50 -0400

    UI screen functionality

---
+++ Unity Project/Space Frog Grappler/Assets/Scripts/UI/S_CompleteScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static System.Net.Mime.MediaTypeNames;
+using Application = UnityEngine.Application;
+
+public class S_CompleteScreen : MonoBehaviour
+{
+    public S_SceneSwitcher SceneSwitcher;
+    public SceneInfo SceneIndexes;
+    Gamepad gamepad = Gamepad.current;
+
+    void Update()
+    {
+        if (gamepad != null)
+            gamepad.SetMotorSpeeds(0, Mathf.Sin(Time.time * 4.5f) + 0.2f); //Makes tongue button pulse
+
+        if (Input.GetButtonDown("Tongue"))
+        {
+            StartCoroutine(SceneSwitcher.LoadScene(SceneIndexes.SelectLevel));
+        }
+    }
+}
