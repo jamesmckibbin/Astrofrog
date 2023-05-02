45f2746ef33a43c2694c8f507f91fb6303df144e
isaac.eddy <isaac.eddy@mymail.champlain.edu>
2023-05-02 15:09:37 -0400

    -Adding scriptable object creating multiple character types

---
+++ Unity Project/Space Frog Grappler/Assets/Scripts/ScripObjs/FrogData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FrogData : ScriptableObject
+{
+   public float tongueSpeed;
+   public float pullSpeed;
+   public float ligma;
+
+   public Sprite frogIdle;
+   public Sprite frogStretch;
+}
