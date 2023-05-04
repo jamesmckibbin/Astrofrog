9c4c9ae28ab958671b56714669001026703b23c5
Isaac Eddy <isaac.eddy@mymail.champlain.edu>
2023-05-03 22:34:38 -0400

    -Buf gixes

--- Unity Project/Space Frog Grappler/Assets/Scripts/Flies/S_TwoWayFly.cs
+++ Unity Project/Space Frog Grappler/Assets/Scripts/Flies/S_TwoWayFly.cs
@@ -7,13 +7,17 @@ public class S_TwoWayFly : MonoBehaviour
     //Each instance of these flies can have their speed and start direction tweaked in the editor.
     [SerializeField] private float movementSpeed, movementAngle;
     private float translatedX, translatedY;
-    private Rigidbody2D flyRb; 
+    private Rigidbody2D flyRb;
+    SpriteRenderer flySpriteRender;
+    public CircleCollider2D col;
 
     //Uses trigonometrics to translate the speed and angle into two vectors.
     //Wouldn't ya know it? That stuff we were taught in high-school geometry class had real-life applications after all!
     void Start()
     {
         flyRb = GetComponent<Rigidbody2D>();
+        flySpriteRender = GetComponent<SpriteRenderer>();
+        col = GetComponent<CircleCollider2D>();
 
         translatedX = movementSpeed * Mathf.Cos(movementAngle * Mathf.Deg2Rad);
         translatedY = movementSpeed * Mathf.Sin(movementAngle * Mathf.Deg2Rad);
