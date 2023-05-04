752bbe42897d65af8fdc6f7a1132455089621e34
isaac.eddy <isaac.eddy@mymail.champlain.edu>
2023-05-04 14:09:54 -0400

    -moving flies now look where they're going

--- Unity Project/Space Frog Grappler/Assets/Scripts/Flies/S_TwoWayFly.cs
+++ Unity Project/Space Frog Grappler/Assets/Scripts/Flies/S_TwoWayFly.cs
@@ -10,7 +10,6 @@ public class S_TwoWayFly : MonoBehaviour
     private Rigidbody2D flyRb;
     SpriteRenderer flySpriteRender;
     public CircleCollider2D col;
-
     //Uses trigonometrics to translate the speed and angle into two vectors.
     //Wouldn't ya know it? That stuff we were taught in high-school geometry class had real-life applications after all!
     void Start()
@@ -22,7 +21,7 @@ public class S_TwoWayFly : MonoBehaviour
         translatedX = movementSpeed * Mathf.Cos(movementAngle * Mathf.Deg2Rad);
         translatedY = movementSpeed * Mathf.Sin(movementAngle * Mathf.Deg2Rad);
 
-        flyRb.velocity = new Vector2 (translatedX, translatedY);
+        flyRb.velocity = new Vector2(translatedX, translatedY);
     }
 
     //When this fly hits a wall, it'll change directions.
@@ -33,7 +32,17 @@ public class S_TwoWayFly : MonoBehaviour
             translatedX *= -1;
             translatedY *= -1;
 
-            flyRb.velocity = new Vector2 (translatedX, translatedY);
+            flyRb.velocity = new Vector2(translatedX, translatedY);
+
+            if (flyRb.velocity.x > 0)
+            {
+                transform.localScale = new Vector3(-0.4f, 0.4f, 0.4f);
+            }
+            else if (flyRb.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+
+            }
         }
     }
 }
